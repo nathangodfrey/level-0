4cda95792bf718b7ecf3bd7d8aadcf73b57101cf
nathangodfrey <league@Mac-mini-2.local>
2016-10-06 17:57:32 -0700

    robot square and triangle to make a house

---
+++ eclipse/src/level0/house.java
@@ -0,0 +1,23 @@
+package level0;
+
+import org.jointheleague.graphical.robot.*;
+
+public class house {
+public static void main(String[] args) {
+Robot harry=new Robot();
+harry.penDown();
+harry.setSpeed(10);
+for(int i=0;i<5; i=i+1){
+harry.move(300);
+harry.turn(90);
+harry.move(300);
+harry.turn(90);
+harry.move(300);
+harry.turn(90);
+harry.move(300);
+harry.turn(90);
+
+
+
+}
+}
