ce96fc5ee03d5e06c0a55ae9c1feb69ad3950c96
nathangodfrey <league@Mac-mini-2.local>
2016-10-13 17:58:10 -0700

    made house which is in current progress

--- eclipse/src/level0/house.java
+++ eclipse/src/level0/house.java
@@ -3,21 +3,20 @@ package level0;
 import org.jointheleague.graphical.robot.*;
 
 public class house {
-public static void main(String[] args) {
-Robot harry=new Robot();
-harry.penDown();
-harry.setSpeed(10);
-for(int i=0;i<5; i=i+1){
-harry.move(300);
-harry.turn(90);
-harry.move(300);
-harry.turn(90);
-harry.move(300);
-harry.turn(90);
-harry.move(300);
-harry.turn(90);
+	public static void main(String[] args) {
+		Robot harry = new Robot();
+		harry.penDown();
+		harry.setSpeed(10);
+		for (int i = 0; i < 5; i = i + 1) {
+			harry.move(300);
+			harry.turn(90);
+			harry.move(300);
+			harry.turn(90);
+			harry.move(300);
+			harry.turn(90);
+			harry.move(300);
+			harry.turn(90);
 
-
-
-}
+		}
+	}
 }
